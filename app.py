92a684d2319626a31dacf0d1fe3a9ad221e36b7f
Jeffrey Chen <jeffreyhc@berkeley.edu>
2015-01-07 15:42:23 -0800

    Added draft JSON input and output via comment

--- app.py
+++ app.py
@@ -7,5 +7,18 @@ app = Flask(__name__)
 def index():
     return "Hello, World!"
 
+#Input JSON 
+# {"receipt": "[some file]"}
+# Return JSON 
+# {"receiptID" : "[unique identifier for the receipt]" , 
+	# "userID": "[unique identifier of who posted it]", 
+	# "date" :"[date of transaction]", 
+	# "totalCost" :"[cost]",
+	# "items": {"name": "[name]", "cost": "[$$}"}, 
+	# 		{"name": "[name]", "cost": "[$$}"}, 
+	# 		{"name": "[name]", "cost": "[$$}"}, 
+	# 		{"name": "[name]", "cost": "[$$}"}}
+#
+
 if __name__ == '__main__':
     app.run(debug=True)
