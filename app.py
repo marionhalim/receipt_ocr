6165db359175841d5c0bea5f65db979444549bc4
Jeffrey Chen <jeffreyhc@berkeley.edu>
2015-01-08 15:38:36 -0800

    JSON format done
    Need to handle all image cases

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 #!flask/bin/python
 from flask import Flask
+from flask import request
+from flask import jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
-
+#ALL ROUTES 
 @app.route('/healthCheck', methods=['GET'])
 def healthCheck():
 	return "Health Check OK"
@@ -16,6 +15,7 @@ def healthCheck():
 # Return JSON 
 # {"receiptID" : "[unique identifier for the receipt]" , 
 	# "userID": "[unique identifier of who posted it]", 
+	# "originalImage: "[original Image URL]",
 	# "vendor": "[vender of the transaction]",
 	# "date" :"[date of transaction]", 
 	# "totalCost" :"[cost]",
@@ -24,6 +24,20 @@ def healthCheck():
 	# 		{"name": "[name]", "cost": "[$$}"}, 
 	# 		{"name": "[name]", "cost": "[$$}"}}
 #
+@app.route('/ocr', methods=['POST'])
+def ocr():
+	print "HERE"
+	if request.headers ['Content-Type'] == 'application/json': 
+		#Run all the OCR stuff should just take an image URL 
+		print "DERP"
+		originalImage = request.json["receipt"]
+		items = []
+		item = {'item': "",
+				'cost': ""}
+		items.append(item)
+		return jsonify({"originalImage": originalImage, 'vendor': "Costco", 
+			"date": "01/08/2014", "totalCost" :"$200", "items": items}), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
