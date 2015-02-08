3459243ddf9df27243e6b187850b15163c1909da
Jeffrey Chen <jeffreyhc@berkeley.edu>
2015-02-08 10:19:40 -0800

    Added a receipt class

---
+++ app/receipt.py
@@ -0,0 +1,33 @@
+"""A class for receipts"""
+import json
+
+class receipt(): 
+
+	def __init__(self, file_path):
+		self.receipt = file_path
+		self.userID = ""
+		self.transactionID = ""
+		self.vendor = ""
+		self.date = ""
+		self.totalCost = "" 
+		self.items = {}
+
+	""" @param item 
+		@param cost 
+		@return boolean"""
+	def addItem(self, item, cost):
+		self.items[item] = cost
+		return True
+
+	"""@return a JSON string of the receipt"""
+	def toJSON(self):
+		return 
+
+
+
+	"""Method to validate file path"""
+
+
+	"""Method to validate userID"""
+
+	"""Method to validate a transactionID""" 
